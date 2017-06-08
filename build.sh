fdb6a79f19b3ef77405ed64831e304dd63155cee
Robert P <robisys@users.noreply.github.com>
2017-06-08 11:24:44 +0200

    Create build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # dotnet restore && dotnet build
 # dotnet new --info
-dotnet  --info
-dotnet  new --help
-dotnet  test --help
+#dotnet  --info
+#dotnet  new --help
+#dotnet  test --help
 # chmod +x ./src/bsp1/build.sh
 #cd src/bsp1
 cd src/bsp2
